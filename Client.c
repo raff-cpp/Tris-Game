f56e935aa11bf1a22989944db640c45a83d5211d
Raffaele Vergara <vergara.raffaele123@gmail.com>
2026-03-19 17:13:31 +0100

    Quinto commit

--- Client.c
+++ Client.c
@@ -7,6 +7,7 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <sys/select.h>
 
 #define BUFFER_SIZE 4096
 #define DEFAULT_PORT 8080
@@ -75,26 +76,50 @@ int main() {
 
 
     printf("Connesso al server %s:%s\n", server_host, server_port_str);
-        // Ciclo di comunicazione con il server
+    
+    // Ciclo di comunicazione con il server
     while (1) {
-        // Legge la risposta del server
-        ssize_t bytes_received = recv(sockfd, buffer, sizeof(buffer) - 1, 0);
-        if (bytes_received < 0) {
-            perror("Errore nella ricezione dei dati");
-            break;
-        } else if (bytes_received == 0) {
-            printf("Connessione chiusa dal server\n");
-            break;
-        }
-
-        buffer[bytes_received] = '\0'; // Assicura che il buffer sia null-terminated
-        printf("Messaggio ricevuto dal server: %s\n", buffer);
-        // Invia una risposta al server (ad esempio, un semplice messaggio di conferma)
-        const char *response = "Messaggio ricevuto";
-        ssize_t bytes_sent = send(sockfd, response, strlen(response), 0);
-        if (bytes_sent < 0) {
-            perror("Errore nell'invio dei dati");
+        // Legge la risposta del server (se disponibile)
+        fd_set readfds;
+        struct timeval tv;
+        FD_ZERO(&readfds);
+        FD_SET(sockfd, &readfds);
+        FD_SET(STDIN_FILENO, &readfds);
+        
+        tv.tv_sec = 0;
+        tv.tv_usec = 100000;  // 100ms timeout
+        
+        int activity = select(sockfd + 1, &readfds, NULL, NULL, &tv);
+        
+        if (activity < 0) {
+            perror("Errore in select");
             break;
         }
-}
+        
+        // Se il server ha inviato dati
+        if (FD_ISSET(sockfd, &readfds)) {
+            ssize_t bytes_received = recv(sockfd, buffer, sizeof(buffer) - 1, 0);
+            if (bytes_received < 0) {
+                perror("Errore nella ricezione dei dati");
+                break;
+            } else if (bytes_received == 0) {
+                printf("Connessione chiusa dal server\n");
+                break;
+            }
+            buffer[bytes_received] = '\0';
+            printf("%s", buffer);  // Stampa il messaggio del server così com'è
+        }
+        
+        // Se l'utente ha digita qualcosa da stdin
+        if (FD_ISSET(STDIN_FILENO, &readfds)) {
+            char user_input[BUFFER_SIZE];
+            if (fgets(user_input, sizeof(user_input), stdin) != NULL) {
+                ssize_t bytes_sent = send(sockfd, user_input, strlen(user_input), 0);
+                if (bytes_sent < 0) {
+                    perror("Errore nell'invio dei dati");
+                    break;
+                }
+            }
+        }
+    }
 }
